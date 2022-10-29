9badc9f707fd58e933ecfcc7ac242659a16a0f6b
yochan0103 <sky.infinity11@gmail.com>
2022-10-29 17:23:10 +0900

    サイト遷移時のフェードイン/アウトを実装

--- pages/Concept-page.js
+++ pages/Concept-page.js
@@ -1,6 +1,15 @@
+import { motion } from "framer-motion"
+import styles from "./styles/Concept-page.module.css"
+
 const ConceptPage = () => {
     return (
-        <h1>コンセプトページです。</h1>
+        <motion.div
+            initial={{opacity:0}}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}  
+        >
+            <h1 className={styles.title}>コンセプトページです。</h1>
+        </motion.div>
     )
 }
 
